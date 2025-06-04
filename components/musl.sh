6587e1968d3e9a0789eaeb7a624ee4df70cd8872
Charles <c4a@monocles.eu>
2025-06-04 15:17:37 -0400

    Add basic SBOM

--- components/musl.sh
+++ components/musl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git clone --depth=1 --single-branch --branch=master git://git.musl-libc.org/musl
 cd musl
+echo musl=$(git rev-parse HEAD) > .sbom-fragment
 ./configure
 make -j$(nproc)
 cd ..
 cp musl/lib/libc.so rootfs/lib/ld-musl-$(uname -m).so.1
+cat musl/.sbom-fragment >> rootfs/.sbom
 rm -rf musl
--- components/sbase.sh
+++ components/sbase.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 git clone --depth=1 --single-branch --branch=master git://git.suckless.org/sbase
 cd sbase
+echo sbase=$(git rev-parse HEAD) > .sbom-fragment
 make -j$(nproc)
 cd ..
 find sbase -maxdepth 1 -type f -executable -exec cp {} rootfs/bin \;
+cat sbase/.sbom-fragment >> rootfs/.sbom
 rm -rf sbase
