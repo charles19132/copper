4c29ddb4942f0a2404b28a0b9dc648bece035264
Charles <c4a@monocles.eu>
2025-06-05 15:50:35 -0400

    Even better error handling

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 FLAGS="-s -Os -ffunction-sections -fdata-sections -Wl,--gc-sections"
 export CFLAGS=$FLAGS
 export CXXFLAGS=$FLAGS
-rm -rf rootfs || true
+test -d rootfs && rm -rf rootfs
 mkdir -p rootfs/{bin,lib}
 . components/musl.sh
 . components/dash.sh
